d2e3ae9f97bcfbefa6eb751eb7e287206755d384
sb633056 <sb633056@gmail.com>
2025-12-06 09:26:01 +0530

    Delete frontend/app.py

--- frontend/app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from smartbrain_single_file_system_v13_4_Integration_patch import run_smartbrain_ui
-
-st.set_page_config(page_title="SmartBrain Founder AI", layout="wide")
-run_smartbrain_ui()
